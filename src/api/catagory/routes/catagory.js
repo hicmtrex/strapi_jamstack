c029758c582dd7131442b01e1aa14b69ea28d0fc
Firstname Lastname <your_email@youremail.com>
2022-01-27 05:43:45 +0100

    pushing

---
+++ src/api/catagory/routes/catagory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catagory router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::catagory.catagory');
---
+++ src/api/catagory/services/catagory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catagory service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::catagory.catagory');
